d19fbb2aa647f6bab12fa44869563e3ab55d1e75
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-20 18:16:27 +0000

    feat: register component

--- src/pages/register/register.component.jsx
+++ src/pages/register/register.component.jsx
@@ -1,7 +1,27 @@
 import './register.style.scss'
 
 const Register = () => {
-  return <div className='register'>Register</div>
+  return <div className='register'>
+    <div className="card">
+      <div className="left">
+        <h1>Spec</h1>
+        <h1>Trum.</h1>
+        <p>A social media website for smart people, who love being a part of a vibrant community, spread positivity and most importantly, comfortable in their own skin.</p>
+        <span>Do you have an account?</span>
+        <button>Login</button>
+      </div>
+      <div className="right">
+        <h1>Register</h1>
+        <form>
+          <input type="text" placeholder='Username' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <input type="name" placeholder='Name' />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  </div>
 }
 
 export default Register
