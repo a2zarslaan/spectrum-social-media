907bdcef0b8f90d023b7f8333a2c3ad04b6578d9
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-20 18:09:26 +0000

    feat: site description on home page

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -4,13 +4,14 @@ const Login = () => {
   return <div className='login'>
     <div className="card">
       <div className="left">
-        <h1>Hello World.</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit vero rem quas doloremque placeat aperiam nemo architecto. Quae dolor maiores nulla mollitia aspernatur tempore unde, est odio nemo.</p>
+        <h1>Spec</h1>
+        <h1>Trum.</h1>
+        <p>A social media website for smart people, who love being a part of a vibrant community, spread positivity and most importantly, comfortable in their own skin.</p>
         <span>Don't have an account?</span>
         <button>Register</button>
       </div>
       <div className="right">
-        <h1>Login</h1>
+        <h1>Welcome back! Login</h1>
         <form>
           <input type="text" placeholder='Username' />
           <input type="password" placeholder='Password' />
