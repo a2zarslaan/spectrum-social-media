3658c67cca0f5677e4148d0ab4ed5d8340c964fc
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-21 17:00:25 +0000

    feat: navbar styles

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ import Profile from "./pages/profile/profile.component"
 
 function App() {
 
-  const currentUser = false;
+  const currentUser = true;
 
   const Layout = () => {
     return (<div>
       <Navbar />
       <div style={{display: "flex"}}>
         <Leftbar />
-        <Outlet />
+        <div style={{flex: 6}}>
+          <Outlet />
+        </div>
         <Rightbar />
       </div>
     </div>)
