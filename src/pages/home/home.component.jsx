9e50fe4d47d0928f616cec23a502e59a23bbeab0
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-22 17:12:34 +0000

    feat: ui for stories

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,7 +1,12 @@
+import Posts from '../../components/posts/posts.component'
+import Stories from '../../components/stories/stories.component'
 import './home.style.scss'
 
 const Home = () => {
-  return <div className='home'>Home</div>
+  return <div className='home'>
+    <Stories />
+    <Posts />
+  </div>
 }
 
 export default Home
