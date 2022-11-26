f731e5e348a938e5500d18096cd0a6d0b3793aaa
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-26 16:15:06 +0000

    feat: connect with mysql database

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import authRoutes from "./routes/auth.js"
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(8800, () => {
+  console.log("API working");
+})
