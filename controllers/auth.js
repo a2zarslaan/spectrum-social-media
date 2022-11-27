b0ef16de46f8beb2bdc3a44cf352e4dc6a786f86
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-27 13:11:19 +0000

    feat: add user to db on registeration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,52 @@
-import { db } from "../connect.js"
+import { db } from '../connect.js';
+import bcrypt from 'bcryptjs';
 
 export const register = (req, res) => {
-  // check if user exists
+	// check if user exists
 
-  const q = "SELECT FROM users WHERE username = ?"
+	const q = 'SELECT * FROM users WHERE username = ?';
 
-  db.query(q, [req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
-    if (data.length) return res.status(409).json("user already exists!");
-  });
+	db.query(q, [req.body.username], (err, data) => {
+		if (err) return res.status(500).json(err);
+		if (data.length) return res.status(409).json('user already exists!');
 
+		// create new user
+		//  hash the password
+		const salt = bcrypt.genSaltSync(10);
+		const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-  // create new user
-  //  hash the password
-}
+		const q =
+			'INSERT INTO users (`username`, `email`, `password`,`name`) VALUE (?)';
 
+		const values = [
+			req.body.username,
+			req.body.email,
+			hashedPassword,
+			req.body.name,
+		];
+
+		db.query(q, [values], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('User has been created.');
+		});
+	});
+};
 
 export const login = (req, res) => {
+	const q = 'SELECT * FROM users WHERE username =?';
 
-}
+	db.query(q, [req.body.username], (err, data) => {
+		if (err) return res.status(500).json(err);
+		if (data.length === 0) return res.status(404).json('User not found!');
 
+		const checkPassword = bcrypt.compareSync(
+			req.body.password,
+			data[0].password
+		);
 
-export const logout = (req, res) => {
+		if (!checkPassword)
+			return res.status(400).json('Wrong password for username');
+	});
+};
 
-}
+export const logout = (req, res) => {};
