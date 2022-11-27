6c1787c068136cce5b43a3723f3ae415fe33d98b
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-27 13:15:49 +0000

    feat: import cors and cookie parser middlewares

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ import postRoutes from './routes/posts.js';
 import commentRoutes from './routes/comments.js';
 import likeRoutes from './routes/likes.js';
 import authRoutes from './routes/auth.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser);
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
