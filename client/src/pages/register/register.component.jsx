119e5c24d85af00a3693a3506bceffcd911f771a
a2zarslaan <a2zarslaan@hotmail.com>
2022-11-28 13:13:57 +0000

    feat: register user from client

--- client/src/pages/register/register.component.jsx
+++ client/src/pages/register/register.component.jsx
@@ -1,31 +1,84 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-import './register.style.scss'
+import axios from 'axios';
+
+import './register.style.scss';
 
 const Register = () => {
-  return <div className='register'>
-    <div className="card">
-      <div className="left">
-        <h1>Spec</h1>
-        <h1>Trum.</h1>
-        <p>A social media website for smart people, who love being a part of a vibrant community, spread positivity and most importantly, comfortable in their own skin.</p>
-        <span>Do you have an account?</span>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </div>
-      <div className="right">
-        <h1>Register</h1>
-        <form>
-          <input type="text" placeholder='Username' />
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
-          <input type="name" placeholder='Name' />
-          <button>Register</button>
-        </form>
-      </div>
-    </div>
-  </div>
-}
+	const [inputs, setInputs] = useState({
+		username: '',
+		email: '',
+		password: '',
+		name: '',
+	});
 
-export default Register
+	const [err, setErr] = useState(null);
+
+	const handleChange = (e) => {
+		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+	const handleClick = async (e) => {
+		e.preventDefault();
+
+		try {
+			await axios.post('http://localhost:8800/api/auth/register', inputs);
+		} catch (err) {
+			setErr(err.response.data);
+		}
+	};
+
+	console.log(err);
+	return (
+		<div className='register'>
+			<div className='card'>
+				<div className='left'>
+					<h1>Spec</h1>
+					<h1>Trum.</h1>
+					<p>
+						A social media website for smart people, who love being a part of a
+						vibrant community, spread positivity and most importantly,
+						comfortable in their own skin.
+					</p>
+					<span>Do you have an account?</span>
+					<Link to='/login'>
+						<button>Login</button>
+					</Link>
+				</div>
+				<div className='right'>
+					<h1>Register</h1>
+					<form>
+						<input
+							type='text'
+							placeholder='Username'
+							name='username'
+							onChange={handleChange}
+						/>
+						<input
+							type='email'
+							placeholder='Email'
+							name='email'
+							onChange={handleChange}
+						/>
+						<input
+							type='password'
+							placeholder='Password'
+							name='password'
+							onChange={handleChange}
+						/>
+						<input
+							type='name'
+							placeholder='Name'
+							name='name'
+							onChange={handleChange}
+						/>
+						{err && err}
+						<button onClick={handleClick}>Register</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
