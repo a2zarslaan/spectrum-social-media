5f87e7625e65a90facb635d22a1867ca63917e1d
a2zarslaan <a2zarslaan@hotmail.com>
2022-12-01 17:20:47 +0000

    feat: post new comments

--- client/src/components/comments/comments.component.jsx
+++ client/src/components/comments/comments.component.jsx
@@ -1,13 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
 
 import './comments.style.scss';
 
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { makeRequest } from '../../axios';
 import moment from 'moment';
 
 const Comments = ({ postId }) => {
+	const [desc, setDesc] = useState('');
 	const { currentUser } = useContext(AuthContext);
 
 	const { isLoading, error, data } = useQuery(['comments'], () =>
@@ -16,18 +17,41 @@ const Comments = ({ postId }) => {
 		})
 	);
 
+	const queryClient = useQueryClient();
+
+	const mutation = useMutation({
+		mutationFn: (newComment) => {
+			return makeRequest.post('/comments', newComment);
+		},
+		onSuccess: () => {
+			// Invalidate and refetch
+			queryClient.invalidateQueries({ queryKey: ['comments'] });
+		},
+	});
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		mutation.mutate({ desc, postId });
+		setDesc('');
+	};
+
 	return (
 		<div className='comments'>
 			<div className='write'>
 				<img src={currentUser.profilePic} alt='' />
-				<input type='text' placeholder='Write a comment' />
-				<button>Send</button>
+				<input
+					type='text'
+					placeholder='Write a comment'
+					value={desc}
+					onChange={(e) => setDesc(e.target.value)}
+				/>
+				<button onClick={handleClick}>Send</button>
 			</div>
 			{isLoading
 				? 'Loading...'
 				: data.map((comment) => (
-						<div className='comment'>
-							<img src={comment.profilePicture} alt='' />
+						<div className='comment' key={comment.id}>
+							<img src={comment.profilePic} alt='' />
 							<div className='info'>
 								<span>{comment.name}</span>
 								<p>{comment.desc}</p>
